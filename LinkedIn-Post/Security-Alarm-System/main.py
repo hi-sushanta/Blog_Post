dd148b841226f286d146cfa8ea05389bd92ba0a5
hi-sushanta <iamchi@skiff.com>
2024-02-23 08:32:57 +0530

    Add Security alarm system

---
+++ LinkedIn-Post/Security-Alarm-System/main.py
@@ -0,0 +1,108 @@
+import torch
+import numpy as np
+import cv2
+from time import time
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator, colors
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from password import *
+
+password = password 
+from_email = from_email  # must match the email used to generate the password
+to_email = to_email  # receiver email
+
+server = smtplib.SMTP('smtp.gmail.com: 587')
+server.starttls()
+server.login(from_email, password)
+
+def send_email(to_email, from_email, img,object_detected=1):
+    message = MIMEMultipart()
+    message['From'] = from_email
+    message['To'] = to_email
+    message['Subject'] = "Security Alert"
+    # Add in the message body
+    message_body = f'ALERT - {object_detected} objects has been detected!!'
+    message.attach(MIMEText(message_body, 'plain'))
+    _, image_bytes = cv2.imencode('.jpg', img)  # Encode the image as a JPEG
+    image = MIMEImage(image_bytes.tobytes())
+    image.add_header('Content-ID', '<image>')  # Set a content ID for embedding
+    message.attach(image)
+
+    server.sendmail(from_email, to_email, message.as_string())
+
+class ObjectDetection:
+    def __init__(self, capture_index):
+        # default parameters
+        self.capture_index = capture_index
+        self.email_sent = False
+
+        # model information
+        self.model = YOLO("yolov8n.pt")
+
+        # visual information
+        self.annotator = None
+        self.start_time = 0
+        self.end_time = 0
+
+        # device information
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    def predict(self, im0):
+        results = self.model(im0)
+        return results
+
+    def display_fps(self, im0):
+        self.end_time = time()
+        fps = 1 / np.round(self.end_time - self.start_time, 2)
+        text = f'FPS: {int(fps)}'
+        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1.0, 2)[0]
+        gap = 10
+        cv2.rectangle(im0, (20 - gap, 70 - text_size[1] - gap), (20 + text_size[0] + gap, 70 + gap), (255, 255, 255), -1)
+        cv2.putText(im0, text, (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2)
+
+    def plot_bboxes(self, results, im0):
+        class_ids = []
+        self.annotator = Annotator(im0, 3, results[0].names)
+        boxes = results[0].boxes.xyxy.cpu()
+        clss = results[0].boxes.cls.cpu().tolist()
+        names = results[0].names
+        for box, cls in zip(boxes, clss):
+            class_ids.append(cls)
+            self.annotator.box_label(box, label=names[int(cls)], color=colors(int(cls), True))
+        return im0, class_ids
+
+    def __call__(self):
+        cap = cv2.VideoCapture(self.capture_index)
+        assert cap.isOpened()
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        frame_count = 0
+        while True:
+            self.start_time = time()
+            ret, im0 = cap.read()
+            assert ret
+            results = self.predict(im0)
+            im0, class_ids = self.plot_bboxes(results, im0)
+
+            if len(class_ids) > 0:  # Only send email If not sent before
+                if not self.email_sent:
+                    send_email(to_email, from_email, im0, len(class_ids))
+                    self.email_sent = True
+            else:
+                self.email_sent = False
+
+            self.display_fps(im0)
+            cv2.imshow('YOLOv8 Detection', im0)
+            frame_count += 1
+            key = cv2.waitKey(1)
+            if (key== ord("Q")) and (key == ord("q")) and (key == 27):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+        server.quit()
+
+detector = ObjectDetection(capture_index=0)
+detector()
