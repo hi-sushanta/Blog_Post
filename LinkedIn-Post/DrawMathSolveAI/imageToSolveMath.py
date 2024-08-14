d2648e687bd275fead454be6e56eeed75414a99d
hi-sushanta <hisushanta@proton.me>
2024-08-15 02:36:49 +0530

    remove unnecassary comment

--- LinkedIn-Post/DrawMathSolveAI/imageToSolveMath.py
+++ LinkedIn-Post/DrawMathSolveAI/imageToSolveMath.py
@@ -11,17 +11,10 @@ if uploaded_files is not None:
     # Choose a Gemini model.
     model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
-    # # Upload the file and print a confirmation.
-    # sample_file = genai.upload_file(path="mathproblem.jpg",
-    #                             display_name="Jetpack drawing")
     imageFile = io.BytesIO(uploaded_files.read())
     sample_file = Image.open(imageFile)
-    # print(f"Uploaded file '{sample_file.display_name}' as: {sample_file.uri}")
-        
-
     # Prompt the model with text and the previously uploaded image.
     response = model.generate_content([sample_file, "Solve this math"])
-    # print(">" + response.text)
     col1, col2 = st.columns(2,gap='large')
     with col1:
         st.title("Question")
